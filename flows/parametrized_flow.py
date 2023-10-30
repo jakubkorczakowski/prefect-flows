747f920ff506c0e868165ab323b5f1f23bd47268
jakubkorczakowski <jakubkorczakowski@gmail.com>
2023-10-30 20:36:42 +0100

    make dir

--- flows/parametrized_flow.py
+++ flows/parametrized_flow.py
@@ -36,6 +36,7 @@ def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file."""
     path = Path(f"data/{color}/{dataset_file}.parquet")
+    path.parent.mkdir(exist_ok=True, parents=True)
     df.to_parquet(path, compression='gzip')
     return path
 
